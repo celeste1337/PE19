bdf378aa381ae2445ee73892283bd336e12bbdf8
my7998 <my7998@IGM2435-07>
2018-10-22 12:28:12 -0400

    init files

---
+++ YostCeleste_PE19/YostCeleste_PE19/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YostCeleste_PE19
+{
+    class Node
+    {
+    }
+}
---
+++ YostCeleste_PE19/YostCeleste_PE19/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YostCeleste_PE19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
